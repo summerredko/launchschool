21dbdedd687cb59c0a88478836d03fa203f29ad9
Summer Redko <summerredko@Summers-MacBook-Pro.local>
2024-10-09 11:13:51 -0400

    add calculator program

---
+++ small_problems/calculator.py
@@ -0,0 +1,53 @@
+# Ask the user for the first number.
+# Ask the user for the second number.
+# Ask the user for an operation to perform.
+# Perform the operation on the two numbers.
+
+def prompt(message):
+    print(f"==> {message}")
+
+def invalid_number(number_str):
+    try:
+        int(number_str)
+    except ValueError:
+        return True
+
+    return False
+
+prompt('Welcome to Calculator!')
+
+prompt("What's the first number?")
+number1 = input()
+
+while invalid_number(number1):
+    prompt("Hmm... that doesn't look like a valid number.")
+    number1 = input()
+
+prompt("What's the second number?")
+number2 = input()
+
+while invalid_number(number2):
+    prompt("Hmm... that doesn't look like a valid number.")
+    number2 = input()
+
+prompt("""What operation would you like to perform
+1) Add 2) Subtract 3) Multiply 4) Divide""")
+operation = input()
+
+while operation not in ["1", "2", "3", "4"]:
+    prompt('You must choose 1, 2, 3, or 4')
+    operation = input()
+
+
+match operation:
+    case '1':
+        output = int(number1) + int(number2)
+    case '2':
+        output = int(number1) - int(number2)
+    case '3':
+        output = int(number1) * int(number2)
+    case '4':
+        output = int(number1) // int(number2)
+
+prompt(f'The result is: {output}')
+
