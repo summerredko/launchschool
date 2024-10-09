8c1669125c21537817bff7c2e8b9d28c48739e9a
Summer Redko <summerredko@Summers-MacBook-Pro.local>
2024-10-09 19:01:36 -0400

    add docstrings

--- small_problems/calculator/calculator.py
+++ small_problems/calculator/calculator.py
@@ -1,20 +1,44 @@
+"""
+This module provides a simple calculator that performs basic arithmetic operations.
+It prompts the user for two numbers and an operation, then displays the result.
+
+Functions:
+- prompt(key): Prints a message based on the provided key.
+- invalid_number(number_str): Checks if the provided string is a valid number.
+"""
+
 # Ask the user for the first number.
 # Ask the user for the second number.
-# Ask the user for an operation to perform.
+# # Ask the user for an operation to perform.
 # Perform the operation on the two numbers.
-
-LANGUAGE = 'en'
 
 import json
 
-with open('calculator_messages.json', 'r') as file:
+LANGUAGE = 'en'
+
+with open('calculator_messages.json', 'r', encoding='utf-8') as file:
     MESSAGES = json.load(file)
-    
+
 def prompt(key):
+     """
+    Prints a message based on the provided key.
+
+    Args:
+        key (str): The key to look up the message in the MESSAGES dictionary.
+    """
     message = messages(key, LANGUAGE)
     print(f"==> {message}")
 
 def invalid_number(number_str):
+     """
+    Checks if the provided string is a valid number.
+
+    Args:
+        number_str (str): The string to check.
+
+    Returns:
+        bool: True if the string is not a valid number, False otherwise.
+    """
     try:
         float(number_str)
     except ValueError:
@@ -23,6 +47,16 @@ def invalid_number(number_str):
     return False
 
 def messages(message, lang='en'):
+    """
+    Retrieves a message from the MESSAGES dictionary based on the provided key and language.
+
+    Args:
+        message (str): The key to look up the message.
+        lang (str): The language code (default is 'en').
+
+    Returns:
+        str: The message corresponding to the provided key and language.
+    """
     return MESSAGES[lang][message]
 
 prompt('welcome')
@@ -47,8 +81,8 @@ while True:
     operation = input()
 
     while operation not in ["1", "2", "3", "4"]:
-            prompt("must_choose")
-            operation = input()
+        prompt("must_choose")
+        operation = input()
 
 
     match operation:
@@ -68,7 +102,7 @@ while True:
     answer = input()
     if answer and answer[0].lower() != 'y':
         break
-    
+
 while answer not in ["y", "n"]:
     prompt("invalid_choice")
     answer = input()
